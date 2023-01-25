02d91cbff3792df8aea9cf8b2eb78331650b3748
iovtz <iovitz1999@gmail.com>
2023-01-25 19:23:01 +0800

    feat: 配置vite环境变量

--- src/vue.d.ts
+++ src/vue.d.ts
@@ -1,1 +1,11 @@
-declare module '*.vue' {}
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_BASE_URL: string //定义提示信息 数据是只读的无法被修改
+}
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}

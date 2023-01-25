e10e4de2f5a4a0e490f04b69b394f58ee5348b81
iovtz <iovitz1999@gmail.com>
2023-01-25 19:23:52 +0800

    feat: 登录接口

---
+++ src/stores/user.store.ts
@@ -0,0 +1,27 @@
+import { reactive } from 'vue'
+import { defineStore } from 'pinia'
+import { login } from '@/request'
+import { useRouter } from 'vue-router'
+import { LocalST } from '@/utils/storage'
+import { StorageKey } from '@/const/storage_key'
+
+export const useUserStore = defineStore('user', () => {
+  const router = useRouter()
+  const userInfo = reactive({
+    nickname: '',
+    token: '',
+  })
+  const handleLogin = async (username: string, password: string) => {
+    const res = await login(username, password)
+    if (res.length > 0) {
+      // 登录成功
+      const user = res[0]
+      userInfo.nickname = user.nickname
+      LocalST.set(StorageKey.token, user.token)
+      router.replace('/pages')
+      console.log('跳转')
+    }
+  }
+
+  return { userInfo, handleLogin }
+})
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -7,32 +7,44 @@
           <el-button class="button" text>账号密码登录</el-button>
         </div>
       </template>
-      <el-form :model="form" label-width="60px">
+      <el-form :model="formData" label-width="60px" label-position="left">
         <el-form-item label="用户名">
-          <el-input class="w-96" v-model="form.username" size="large" placeholder="Please input username" clearable />
+          <el-input class="w-96" v-model="formData.username" size="large" placeholder="Please input username" clearable />
         </el-form-item>
         <el-form-item label="密码">
-          <el-input class="w-96" type="password" size="large" placeholder="Please input password" v-model="form.password" clearable />
+          <el-input class="w-96" type="password" size="large" placeholder="Please input password" v-model="formData.password" clearable />
         </el-form-item>
-        <el-form-item>
-          <el-button class="w-full" type="primary" @click="onSubmit">Login</el-button>
-        </el-form-item>
+        <el-button class="w-full" type="primary" @click="handleLogin">Login</el-button>
+        <div class="text-center mt-4">
+          <el-button type="primary" text @click="handleTestLogin">点击使用测试账号登录</el-button>
+        </div>
+
+        <el-alert title="测试账号: admin  测试密码: admin" type="warning" />
       </el-form>
     </el-card>
   </div>
 </template>
 
 <script setup lang="ts">
+import { StorageKey } from '@/const/storage_key'
+import { useUserStore } from '@/stores/user.store'
+import { LocalST } from '@/utils/storage'
 import { reactive } from 'vue'
 
+const userStore = useUserStore()
+
 // do not use same name with ref
-const form = reactive({
+const formData = reactive({
   username: '',
   password: '',
 })
 
-const onSubmit = () => {
-  console.log('submit!')
+const handleLogin = () => {
+  const { username, password } = formData
+  userStore.handleLogin(username, password)
+}
+const handleTestLogin = () => {
+  userStore.handleLogin('admin', 'admin')
 }
 </script>
 
